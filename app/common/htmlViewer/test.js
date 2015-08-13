d3d983743bed88824c1b6e13a8f70be45feba07e
Dennis Haulund Nielsen <dhni@nodes.dk>
2015-08-13 18:31:31 +0200

    LOL JEG ER SKOD :---)

--- app/common/htmlViewer/test.js
+++ app/common/htmlViewer/test.js
@@ -1,14 +1,14 @@
 (function() {
-	//angular.module('demoApp', ['nCore.nMessages']);
+	angular.module('demoApp', ['nCore.nMessages']);
 
-	angular.module('readmir').config(function(nMessagesProvider) {
+	angular.module('demoApp').config(function(nMessagesProvider) {
 		nMessagesProvider.configure({
 			verticalPosition: 'top',
 			horizontalPosition: 'right'
 		});
 	});
 
-	angular.module('readmir').controller('demoCtrl', function($scope, nMessages) {
+	angular.module('demoApp').controller('demoCtrl', function($scope, nMessages) {
 		console.log('heh greetings', $scope)
 		var vm = this;
 		// Demo Methods
@@ -17,6 +17,66 @@
 			console.log('jaerh', nMessages);
 			nMessages.create('The most basic message type available');
 		}
+
+		vm.createBasicMessage = createBasicMessage;
+		vm.createAdvancedMessage = createAdvancedMessage;
+		vm.createCustomMessage = createCustomMessage;
+
+		vm.messageContents = [
+			'Dette er en besked',
+			'Dette er også en besked',
+			'Dette er måske ikke en besked',
+			'Nogen siger dette er en besked, men ingen vil stå ved det',
+			'Ingen ved om dette er en besked',
+			'Denne besked blev lavet i et reagensglas',
+			'Nogen siger dette er en besked, men ingen vil stå ved det, Nogen siger dette er en besked, men ingen vil stå ved det, Nogen siger dette er en besked, men ingen vil stå ved det, Nogen siger dette er en besked, men ingen vil stå ved det'
+		];
+		vm.messageTypes = ['success', 'warning', 'alert', 'info', ''];
+
+		vm.message = {
+			type: 'success',
+			dismissButtonHtml: '&times;',
+			dismissOnClick: true,
+			dismissButton: true,
+			showDismissButton: true,
+			dismissOnTimeout: false,
+			timeout: 3200
+		};
+
+		vm.openMessages = [];
+
+		function createBasicMessage() {
+			nMessages.create('The most basic message type available');
+		}
+
+		function createAdvancedMessage() {
+			nMessages.create({
+				type: 'success',
+				content: 'Such message',
+				dismissButton: true,
+				dismissButtonHtml: '&times;',
+				dismissOnClick: true,
+				dismissOnTimeout: false,
+				timeout: 4000
+			});
+		}
+
+		function createCustomMessage() {
+			var msg = angular.copy(vm.message);
+			msg.content = _getRandomMessage();
+			msg.type = _getRandomType();
+			var msgRef =  nMessages.create(msg);
+			vm.openMessages.push(msgRef);
+		}
+
+		function _getRandomMessage() {
+			return vm.messageContents[Math.floor(Math.random() * vm.messageContents.length)];
+		}
+
+		function _getRandomType() {
+			return vm.messageTypes[Math.floor(Math.random() * vm.messageTypes.length)];
+		}
+
 	});
 
 })();
